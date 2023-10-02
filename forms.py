0b64247a819babf043a8e70926e7849823a76e92
lexie <71516372+lexie21@users.noreply.github.com>
2023-10-02 09:20:34 +0700

    Update forms.py
    changed association

--- forms.py
+++ forms.py
@@ -3,5 +3,5 @@ from wtforms import (StringField,TextField,SubmitField)
 from wtforms.validators import DataRequired
 
 class GateKeeper(FlaskForm):
-    question = StringField('What is the specific term KPers use to denote an action of someone who is about to leave the firm?',validators=[DataRequired()])
+    question = StringField('What is the specific term ers use to denote an action of someone who is about to leave the firm?',validators=[DataRequired()])
     submit = SubmitField('Send it')
